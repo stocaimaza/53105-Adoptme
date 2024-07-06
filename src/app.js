064007d41b1b20e8a1fef00813a4b7b41954f32b
stocaimaza <stocaimaza@hotmail.com>
2024-07-06 12:52:11 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import cors from "cors"; 
 
 const app = express();
 dotenv.config(); 
@@ -14,6 +15,10 @@ const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
